03def938b34005188b752a660b72c7bc1042005c
RoyForce <troyforce001@gmail.com>
2022-05-14 18:05:23 -0400

    echo3d integration

--- src/app/shared/base.service.ts
+++ src/app/shared/base.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ConfigsLoaderService } from '../services/configs-loader.service';
@@ -8,13 +8,26 @@ import { ConfigsLoaderService } from '../services/configs-loader.service';
 })
 export class BaseService {
   private baseUrl = '';
+  private echo3dUrl = '';
   protected httpClient: HttpClient;
   protected _configLoaderService: ConfigsLoaderService
   constructor(private _injector: Injector) {
     this.httpClient = _injector.get(HttpClient);
     this._configLoaderService = _injector.get(ConfigsLoaderService);
     this.baseUrl = this._configLoaderService.ApiUrl;
+    this.echo3dUrl = this._configLoaderService.echo3dUrl;
   }
+
+  public postecho3dC<T>(
+    url: string,
+    data: Object,
+    // options: Object | string,
+    responseType?: Object | string
+  ): Observable<T> {
+    return this.httpClient.post<T>('https://localhost:5001/api/File/UploadEcho3DBlob', data, responseType?responseType:{});
+  }
+
+
 
   public post<T>(
     url: string,
