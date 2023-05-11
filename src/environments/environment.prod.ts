c7ae5720087b2cc43e4b0ee228afb57dc51c8eeb
RoyForce <troyforce001@gmail.com>
2023-05-10 20:55:13 -0500

    fix env test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  baseUrl: window["env"]["apiUrl"] || "",
+  baseUrl: window["env"]["apiUrl"] || "https://app-api.scolptio.com",
   s3ModelUrl: window["env"]["s3ModelUrl"] || "",
   debug: window["env"]["debug"] || false,
-  bucket : window["env"]["bucket"] || "",
+  bucket : window["env"]["bucket"] || "scolptio-crm-bucket",
   region : window["env"]["region"] || "",
   s3AccessKeyId : window["env"]["s3AccessKeyId"] || "",
   s3SecretAccessKey : window["env"]["s3SecretAccessKey"] || "",
